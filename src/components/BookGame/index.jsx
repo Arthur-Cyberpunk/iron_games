9813a1ca8fc508d085e4514097cc98f2f700d691
Arthur Gabriel dos Santos <santos.santos86@hotmail.com>
2023-06-02 17:50:12 -0300

    first commit

--- src/components/BookGame/index.jsx
+++ src/components/BookGame/index.jsx
@@ -7,38 +7,49 @@ import playstation5 from "../../assets/playstation5.png";
 import xboxonex from "../../assets/xboxonex.png";
 import xboxseriess from "../../assets/xboxseriess.png";
 import xboxseriesx from "../../assets/xboxseriesx.png";
+import ModalBookGame from "../ModalBookGame";
 import "./styles.scss";
 
 const BookGame = () => {
   const [game, setGame] = useState("");
   const [error, setError] = useState(false);
+  const [modalOpen, setModalOpen] = useState(false);
+
 
   const handleGame = (event) => {
     setGame(event.target.value);
   };
 
   let imgUrl;
+  let nameGame;
   switch (game) {
     case "xboxseriesx":
       imgUrl = xboxseriesx;
+      nameGame = 'Xbox Series X';
       break;
     case "xboxseriess":
       imgUrl = xboxseriess;
+      nameGame = 'Xbox Series S';
       break;
     case "playstation5":
       imgUrl = playstation5;
+      nameGame = 'Playstation 5';
       break;
     case "xboxonex":
       imgUrl = xboxonex;
+      nameGame = 'Xbox One X';
       break;
     case "playstation4pro":
       imgUrl = playstation4pro;
+      nameGame = 'Playstation 4';
       break;
     case "nintendoswitch":
       imgUrl = nintendoswitch;
+      nameGame = 'Nintendo Switch';
       break;
     default:
       imgUrl = mario;
+      nameGame = ''
   }
 
   const openModal = (event) => {
@@ -47,11 +58,13 @@ const BookGame = () => {
       setError(true);
     } else {
       setError(false);
+      setModalOpen(true)
     }
   };
 
   return (
-    <section className="container">
+    <>
+    <section className="containerBookGame">
       <div className="boxForm">
         <h2>Book a game</h2>
         {error ? <p>Field required!</p> : <></>}
@@ -78,6 +91,9 @@ const BookGame = () => {
       </div>
       <img className="gameImage" src={imgUrl} alt=""></img>
     </section>
+    {modalOpen ? <ModalBookGame imgUrl={imgUrl} nameGame={nameGame}/> : <></> }
+    
+    </>
   );
 };
 
---
+++ src/components/ModalBookGame/index.jsx
@@ -0,0 +1,94 @@
+import { BsXLg } from "react-icons/bs";
+import { FaInfoCircle } from "react-icons/fa";
+import "./styles.scss";
+
+const ModalBookGame = (props) => {
+
+  return (
+    <section className="containerModal">
+      <div className="modalTitle">
+        <h2>Complete Reservation</h2>
+        <BsXLg />
+      </div>
+      <div className="modalMessage">
+        <h4>
+          <FaInfoCircle />
+          &nbsp;Upon completing this reservation enquiry, you will receive:
+        </h4>
+        <p>
+          Your rental voucher to produce on arrival at the rental desk and a
+          toll-free customer support number.
+        </p>
+      </div>
+      <div className="modalInfoModel">
+        <h5>
+          <span>Game - </span>
+          {props.nameGame}
+        </h5>
+        <img src={props.imgUrl}></img>
+      </div>
+      <div className="modalPersonInfo">
+        <h4>Personal Information</h4>
+        <form className="modalForm">
+          <div className="infoForm2column">
+            <span>
+              <label>First Name</label>
+              <input type="text" placeholder="Enter your first name"></input>
+              <p>This field is required.</p>
+            </span>
+            <span>
+              <label>Last Name</label>
+              <input type="text" placeholder="Enter your last name"></input>
+              <p>This field is required.</p>
+            </span>
+            <span>
+              <label>Phone Number</label>
+              <input type="tel" placeholder="Enter your phone number"></input>
+              <p>This field is required.</p>
+            </span>
+            <span>
+              <label>Age</label>
+              <input type="number" placeholder="Enter your age"></input>
+              <p>This field is required.</p>
+            </span>
+          </div>
+          <div className="infoForm1column">
+            <span>
+              <label>Email</label>
+              <input type="email" placeholder="Enter your email"></input>
+              <p>This field is required.</p>
+            </span>
+            <span>
+              <label>Address</label>
+              <input type="text" placeholder="Enter your address"></input>
+              <p>This field is required.</p>
+            </span>
+          </div>
+          <div className="infoForm2column">
+            <span>
+              <label>City</label>
+              <input type="text" placeholder="Enter your city"></input>
+              <p>This field is required.</p>
+            </span>
+            <span>
+              <label>Zip Code</label>
+              <input type="text" placeholder="Enter your zip code"></input>
+              <p>This field is required.</p>
+            </span>
+          </div>
+          <span className="infoCheckbox">
+            <div>
+              <input type='checkbox'></input>
+              <p>Please send me latest news and updates</p>
+            </div>
+            <div>
+              <button>Buy Now</button>
+            </div>
+          </span>
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default ModalBookGame;
