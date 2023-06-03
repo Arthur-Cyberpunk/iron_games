aff86c179bbf218ec57cd480a99c92474117213e
Arthur Gabriel dos Santos <santos.santos86@hotmail.com>
2023-06-03 19:38:56 -0300

    first commit

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,86 @@
+import { useState } from "react";
+import { BsXLg } from "react-icons/bs";
+import { CgMenuRightAlt } from "react-icons/cg";
+import { Link } from "react-router-dom";
+import controle from "../../assets/controle.png";
+import "./styles.scss";
+
+const Header = () => {
+  const [nav, setNav] = useState(false);
+
+  const openNav = () => {
+    setNav(!nav);
+  };
+
+  console.log(nav)
+
+  return (
+    <nav className="containerHeader">
+      <div className="boxLogo">
+        <img className="logoController" src={controle} alt=""></img>
+        <div className="boxNameLogo">
+          <span>Iron</span>
+          <span>Games</span>
+        </div>
+      </div>
+      <div className="boxOptions">
+        <Link to="/" target="_blank" rel="noreferrer">
+          Home
+        </Link>
+        <Link to="/" target="_blank" rel="noreferrer">
+          About
+        </Link>
+        <Link to="/" target="_blank" rel="noreferrer">
+          Testimonials
+        </Link>
+        <Link to="/" target="_blank" rel="noreferrer">
+          Contact
+        </Link>
+      </div>
+      <div className="boxLogin">
+        <Link to="/">Sign in</Link>
+        <Link className="register" to="/">
+          Register
+        </Link>
+      </div>
+      <div className="iconHamburguer" onClick={openNav}>
+        <CgMenuRightAlt />
+      </div>
+      <div className={`menu ${nav ? "open-nav" : ""}`}>
+        <div className="iconX" onClick={openNav}>
+          <BsXLg />
+        </div>
+        <ul>
+          <li>
+            <Link to="/" target="_blank" rel="noreferrer">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/" target="_blank" rel="noreferrer">
+              About
+            </Link>
+          </li>
+          <li>
+            <Link to="/" target="_blank" rel="noreferrer">
+              Testimonials
+            </Link>
+          </li>
+          <li>
+            <Link to="/" target="_blank" rel="noreferrer">
+              Contact
+            </Link>
+          </li>
+          <li>
+            <Link to="/">Sign in</Link>
+            <Link className="register" to="/">
+              Register
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
