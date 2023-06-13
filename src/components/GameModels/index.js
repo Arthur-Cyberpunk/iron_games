fc27facbfaeb4c102908eed1005c773435c9f8d6
Arthur Gabriel dos Santos <santos.santos86@hotmail.com>
2023-06-13 14:44:45 -0300

    first commit

--- src/components/GameModels/index.js
+++ src/components/GameModels/index.js
@@ -3,62 +3,60 @@ import "./styles.scss";
 
 const GameModels = () => {
   return (
-    <section>
-      <div>
-        <div>
-          <div>
-            <h3>Video Game Models</h3>
-            <h2>Our rental fleet</h2>
-            <p>
-              Choose from a variety of our amazing video games to buy for your
-              next gameplay
-            </p>
+    <section className="containerSection">
+      <div className="boxModels">
+        <div className="boxTitle">
+          <h3>Video Game Models</h3>
+          <h2>Our rental fleet</h2>
+          <p>
+            Choose from a variety of our amazing video games to buy for your
+            next gameplay
+          </p>
+        </div>
+        <div className="boxGameContent">
+          <div className="pickGameModel">
+            <button>Xbox Series X</button>
+            <button>Xbox Series S</button>
+            <button>Playstation 5</button>
+            <button>Xbox One X</button>
+            <button>Playstation 4</button>
+            <button>Nintendo Switch</button>
           </div>
           <div>
             <div>
-              <button>Xbox Series X</button>
-              <button>Xbox Series S</button>
-              <button>Playstation 5</button>
-              <button>Xbox One X</button>
-              <button>Playstation 4</button>
-              <button>Nintendo Switch</button>
+              <img></img>
             </div>
             <div>
               <div>
-                <img></img>
+                <span></span>
               </div>
               <div>
                 <div>
+                  <span>Model</span>
                   <span></span>
                 </div>
                 <div>
-                  <div>
-                    <span>Model</span>
-                    <span></span>
-                  </div>
-                  <div>
-                    <span>Mark</span>
-                    <span></span>
-                  </div>
-                  <div>
-                    <span>HD/SSD</span>
-                    <span></span>
-                  </div>
-                  <div>
-                    <span>Teraflops</span>
-                    <span></span>
-                  </div>
-                  <div>
-                    <span>Video</span>
-                    <span></span>
-                  </div>
-                  <div>
-                    <span>Audio</span>
-                    <span></span>
-                  </div>
+                  <span>Mark</span>
+                  <span></span>
                 </div>
-                <Link>Buy Now</Link>
+                <div>
+                  <span>HD/SSD</span>
+                  <span></span>
+                </div>
+                <div>
+                  <span>Teraflops</span>
+                  <span></span>
+                </div>
+                <div>
+                  <span>Video</span>
+                  <span></span>
+                </div>
+                <div>
+                  <span>Audio</span>
+                  <span></span>
+                </div>
               </div>
+              <Link>Buy Now</Link>
             </div>
           </div>
         </div>
